6024e609e632a21d8682775359bd077ed8e1934b
Diogo Fernandes de Oliveira <diogofernandesoliv@gmail.com>
2025-01-07 17:31:43 -0300

    Novas suítes de testes e refatoração utilizando boas práticas com cypress

--- cypress/e2e/exercicio-04.cy.js
+++ cypress/e2e/exercicio-04.cy.js
@@ -1,11 +1,10 @@
 describe('Exercício 04', () => {
-const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTczNjI1MzI1OCwiZXhwIjoxNzM2NTEyNDU4fQ.AQKZcK3O3hWy5l9gbM9AmaARo_lBzc_pqAi_zLLc1ag`;
 
     it('Nome do perfil', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
